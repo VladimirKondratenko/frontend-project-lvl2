6889c4160632729464580f938318708cdf67038a
VladimirKondratenko <v.n.kondratenko@yandex.by>
2022-12-07 00:28:13 +0300

    step 3 (read and show json)

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,24 @@
 #!/usr/bin/env node
 import program from 'commander';
-import { readFileSync } from 'fs';
+import { readFileSync } from 'node:fs';
 
 program
   .version('0.1')  
   .description('Compares two configuration files and shows a difference.')  
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(readFileSync(filepath1, 'utf-8'), readFileSync(filepath2, 'utf-8'))
+  });
 
 program.parse();
-console.log('test');
-// console.log('test', readFileSync(filepath1));
+
+// const options = program.opts();
+// console.log('options', options);
+
+// const path = require('path');
+// path1 = path.resolve("users", "..", "readme.md");
+// console.log('test2', fs.readFileSync('<file1>'));
+// console.log('test', program.parse());
+// console.log('path', path.resolve("file1.json"));
+
---
+++ src/index.js
@@ -0,0 +1,2 @@
+import { readFileSync } from 'fs';
+import _ from 'lodash';
