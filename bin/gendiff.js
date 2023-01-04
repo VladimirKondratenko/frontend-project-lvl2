e606b8711e8e797ab2f0f769cb6490207376e055
VladimirKondratenko <v.n.kondratenko@yandex.by>
2023-01-04 03:23:55 +0300

    step 3

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import program from 'commander';
 import { readFileSync } from 'node:fs';
+import genDiff  from '../src/index.js'
 
 program
   .version('0.1')  
@@ -8,17 +9,8 @@ program
   .arguments('<filepath1> <filepath2>')
   .option('-f, --format [type]', 'output format')
   .action((filepath1, filepath2) => {
-    console.log(readFileSync(filepath1, 'utf-8'), readFileSync(filepath2, 'utf-8'))
+    //console.log(readFileSync(filepath1, 'utf-8'), readFileSync(filepath2, 'utf-8'));
+    genDiff(readFileSync(filepath1, 'utf-8'),readFileSync(filepath2, 'utf-8'));
   });
 
 program.parse();
-
-// const options = program.opts();
-// console.log('options', options);
-
-// const path = require('path');
-// path1 = path.resolve("users", "..", "readme.md");
-// console.log('test2', fs.readFileSync('<file1>'));
-// console.log('test', program.parse());
-// console.log('path', path.resolve("file1.json"));
-
--- src/index.js
+++ src/index.js
@@ -1,2 +1,37 @@
-import { readFileSync } from 'fs';
 import _ from 'lodash';
+
+const genDiff = (firstFile, secondFile) => {
+  const parseFirstFile = JSON.parse(firstFile);
+  const parseSecondFile = JSON.parse(secondFile);
+  const keysFirstFile = Object.keys(parseFirstFile);
+  const keysSecondFile = Object.keys(parseSecondFile);
+  const uniq = _.uniq([...keysFirstFile, ...keysSecondFile]);
+  const sortUniq = _.sortBy(uniq);
+  
+  const result = sortUniq.map(
+    (key) => {
+      const valuesFirstFile = parseFirstFile[key];
+      const valuesSecondFile = parseSecondFile[key];
+
+      if (valuesFirstFile === valuesSecondFile) {
+        return `  ${key} : ${valuesFirstFile}`;
+      }
+      
+      if ((valuesFirstFile && valuesSecondFile) && (valuesFirstFile !== valuesSecondFile)) {
+        return `- ${key} : ${valuesFirstFile} + ${key} : ${valuesSecondFile} `
+      }
+
+      if (valuesFirstFile !== undefined && !valuesSecondFile) {
+        return `- ${key} : ${valuesFirstFile}`;
+      }
+
+      if (!valuesFirstFile && valuesSecondFile !== undefined) {
+        return `+ ${key} : ${valuesSecondFile}`;
+      }
+    }
+  );
+  console.log('result', result);
+  return result; 
+};
+
+export default genDiff;
